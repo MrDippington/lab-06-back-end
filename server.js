f31f5456de0f71734db5e3ca2ff314e5fee482f8
james-ad <muzikjay@gmail.com>
2019-07-30 22:11:43 -0700

    Got server working properly. Added formatted_query property

--- server.js
+++ server.js
@@ -2,23 +2,30 @@
 
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 function Location(city, geoData) {
   this.search_query = city;
-  this.latitude = geoData.results[0].geometry.location.lat;
-  this.longitude = geoData.results[0].geometry.location.lng;
+  this.formatted_address = geoData.results[0].formatted_address;
+  this.latitude = Number(geoData.results[0].geometry.location.lat);
+  this.longitude = Number(geoData.results[0].geometry.location.lng);
 }
 
-// app.use(express.static("./public"));
+app.use(express.static("./public"));
+app.use(cors());
 
 app.get("/location", (request, response) => {
-  const geo = require("./data/geo.json");
-  console.log(request.query.data);
-  const blob = new Location(request.query.data, geo);
-  console.log(blob);
-  response.send(blob);
+  try {
+    const geo = require("./data/geo.json");
+    console.log(request.query.data);
+    const blob = new Location(request.query.data, geo);
+    console.log(blob);
+    response.send(blob);
+  } catch (error) {
+    handleError(error);
+  }
 });
 
 app.get("/data", (request, response) => {
@@ -33,5 +40,12 @@ app.get("/data", (request, response) => {
 app.use("*", (request, response) =>
   response.send("Sorry, that route does not exist.")
 );
+
+function handleError(err, response) {
+  console.error(err);
+  if (response) {
+    response.status(500).send("Sorry, something went wrong here.");
+  }
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
